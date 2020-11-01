e82228acab4b2f419da5d95177edbe67244c97ae
Ludevjhon <70524137+ludevjhon@users.noreply.github.com>
2020-11-01 16:51:20 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -124,6 +124,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+FOD Permissions
+PRODUCT_COPY_FILES += \
+    vendor/bootleggers/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
